3e35a7e15bacb4d9084013686c7c560aed6dd50c
Pdave <ferdinandpdave@gmail.com>
2024-10-26 23:57:22 -0300

    Made the design

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,70 @@
 async function getData() {
-  const response = await fetch("data/data.json");
+  try {
+    const response = await fetch("data/data.json");
 
-  const data = await response.json();
+    const data = await response.json();
 
-  return data;
+    return data;
+  } catch (error) {
+    return console.error("Error: ", error);
+  }
 }
 
-function createCommentElement(comment) {
+function createCommentElement(comment, username) {
   const commentElement = document.createElement("div");
   commentElement.classList.add("comment");
 
-  commentElement.innerHTML = `
-  <div class="user-info">
-    <img src="${comment.user.image.png}" alt="${comment.user.username}" />
-    <span>${comment.user.username}</span>
-    <span>${comment.createdAt}</span>
+  const isCurrentUser = comment.user.username === username;
+  //console.log(isCurrentUser);
+
+  const buttonsChoice = isCurrentUser
+    ? `
+  <div class="delete-edit-btn">
+    <button class="delete-btn">
+      <img src="images/icon-delete.svg" alt="icon-delete" /> Delete
+    </button>
+    <button class="edit-btn">
+      <img src="images/icon-edit.svg" alt="icon-edit" /> Edit
+    </button>
   </div>
-  <p>${comment.content}</p>
-  <span>Score: ${comment.score}</span>
+  `
+    : `
+  <div class="reply-btn">
+    <button>
+      <img src="images/icon-reply.svg" alt="icon reply" />
+      Reply
+    </button>
+  </div>
+  `;
+
+  commentElement.innerHTML = `
+  <div class="comment-element">
+    <div class="score">
+      <img src="images/icon-plus.svg" alt="icon-plus" />
+      <span>${comment.score}</span>
+      <img src="images/icon-minus.svg" alt="icon-minus" />
+    </div>
+    <div>
+      <div class="user-info">
+      <div class="wrapper">
+        <img src="${comment.user.image.png}" alt="${comment.user.username}" />
+        <span class="username">${comment.user.username}</span>
+        <span>${comment.createdAt}</span>
+      </div>
+      <div class="buttons">
+        ${buttonsChoice}
+      </div>
+    </div>
+    <p>${comment.content}</p>
+    </div>
+  </div>
   `;
 
   return commentElement;
 }
 
 function displayUser(userData) {
-  const userElement = document.querySelector(".js-current-user");
+  const userElement = document.querySelector(".js-currentUser-container");
 
   userElement.innerHTML = `
   <div class="current-user">
@@ -46,8 +86,12 @@ async function displayComments() {
 
     if (comment.replies.length > 0) {
       comment.replies.forEach((reply) => {
-        const replyElement = createCommentElement(reply);
+        const replyElement = createCommentElement(
+          reply,
+          data.currentUser.username
+        );
         replyElement.classList.add("reply");
+        replyElement.classList.remove("comment");
         commentElement.appendChild(replyElement);
       });
     }
